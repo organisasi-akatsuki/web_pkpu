8d1c01f1e472a077ad1a5216754cfbf5182310d3
spbudi <accggold20@gmail.com>
2021-06-10 12:45:19 +0700

    sudah fix

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ contents = requests.get(url)
 
 response = bs4.BeautifulSoup(contents.text, "html.parser")
 data = response.find_all('tr', 'table_highlight')
-print(data)
+print(data[0])
 
+"""
 sholat = {}
 i = 0
-for i in data():
+for d in data[0]:
     if i == 1:
         sholat['subuh'] = d.get_text()
     elif i == 2:
@@ -24,4 +25,23 @@ for i in data():
     i += 1
 
 print(sholat)
-#print(sholat['subuh'])
+print(sholat['subuh'])
+"""
+
+sholat = {}
+i = 0
+for d in data[0]:
+    if i == 1:
+        sholat['subuh'] = d.get_text()
+    elif i == 2:
+        sholat['zuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['maghrib'] = d.get_text()
+    elif i == 5:
+        sholat['isya'] = d.get_text()
+    i += 1
+
+print(sholat)
+print(sholat['ashar'])
